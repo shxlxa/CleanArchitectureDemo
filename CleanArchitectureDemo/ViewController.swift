aa9555a9d6768683ec48a3718d812223bb969b76
cft <842495841@qq.com>
2026-07-05 22:28:16 +0800

    Initial Commit

---
+++ CleanArchitectureDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanArchitectureDemo
+//
+//  Created by cft on 2026/7/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
